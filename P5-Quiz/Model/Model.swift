cd4585f9a4edf882d9645c3a429a06f3665c4d04
Adrian <adrian@Adrians-Mac.local>
2018-11-25 10:40:09 +0100

    Done with new token

--- P5-Quiz/Model/Model.swift
+++ P5-Quiz/Model/Model.swift
@@ -11,8 +11,8 @@ import UIKit
 
 class Model{
     
-    //https://quiz2019.herokuapp.com/api/users?token=15285c6fd5a9426d00fb
-    //https://quiz2019.herokuapp.com/api/quizzes?token=15285c6fd5a9426d00fb
+    //https://quiz2019.herokuapp.com/api/users?token=acc820fa0f4d47c1fd3c
+    //https://quiz2019.herokuapp.com/api/quizzes?token=acc820fa0f4d47c1fd3c
     static let apiURL = "https://quiz2019.herokuapp.com/api"
     static let usersURL = "/users"
     static let quizzesURL = "/quizzes"
